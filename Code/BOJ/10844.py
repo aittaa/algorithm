bb87ed77554499f23a3eab8a14b37378b3631440
hyunkim <68850889+aittaa@users.noreply.github.com>
2022-08-01 20:48:29 +0900

    쉬운 계단 수

--- Code/BOJ/10844.py
+++ Code/BOJ/10844.py
@@ -19,3 +19,19 @@ INF = float('inf')
 
 ###
 
+
+N = int(input())
+DP = [[0] * 10 for _ in range(N)]
+
+for i in range(1, 10):
+    DP[0][i] = 1
+
+for i in range(1, N):
+    for j in range(10):
+        up = j + 1
+        down = j - 1
+    
+        DP[i][j] += DP[i-1][up] if up < 10 else 0
+        DP[i][j] += DP[i-1][down] if down >= 0 else 0
+
+print(sum(DP[N-1]) % 1000000000)
